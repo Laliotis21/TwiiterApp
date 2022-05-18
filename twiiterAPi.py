5aa46584f63fc298f43a312bf5bdd81ca7ed57d5
Laliotis Nikos <81807513+Laliotis21@users.noreply.github.com>
2022-05-19 01:01:20 +0300

    s

--- twiiterAPi.py
+++ twiiterAPi.py
@@ -1,4 +1,5 @@
-import pandas as pd 
+from numpy import place
+import pandas as pd
 from dateutil.relativedelta import relativedelta
 import time
 from ast import While
@@ -31,31 +32,31 @@ def twitter_method(query, next_token):
     # final_time = given_time + relativedelta(minutes=n)
     # datetime_new = datetime_new + timedelta(minutes = minutes_to_add)
 
-
     # get tweets from the API
     if not next_token:
 
         tweets = client.search_recent_tweets(query=query,
-                                            tweet_fields=[
-                                                "created_at", "text", "source", "geo"],
-                                            user_fields=[
-                                                "name", "username", "location", "verified", "description", "entities", "id"],
-                                            place_fields=["country"],
-                                            max_results=100,
-                                            expansions='author_id'
-                                            )
+                                             tweet_fields=[
+                                                 "created_at", "text", "source", "geo"],
+                                             user_fields=[
+                                                 "name", "username", "location", "verified", "description", "entities", "id"],
+                                             place_fields=["country", "geo"],
+                                             max_results=10,
+                                             expansions='author_id'
+                                             )
 
     else:
         tweets = client.search_recent_tweets(query=query,
-                                            next_token=["next_token"],
-                                            tweet_fields=[
-                                                "created_at", "id","text", "source", "geo"],
-                                            user_fields=[
-                                                "name", "username", "location", "verified", "description", "entities", "id"],
-                                            place_fields=["country"],
-                                            max_results=100,
-                                            expansions='author_id'
-                                            )
+                                             next_token=["next_token"],
+                                             tweet_fields=[
+                                                 # "created_at", "id","text", "source", "geo"],
+                                                 "attachments", "author_id", "context_annotations", "conversation_id", "created_at", "entities", "geo", "id", "in_reply_to_user_id", "lang non_public_metrics", "organic_metrics", "possibly_sensitive", "promoted_metrics", "public_metrics referenced_tweets", "reply_settings", "source", "text", "withheld"],
+                                             user_fields=[
+                                                 "name", "username", "location", "verified", "description", "entities", "id"],
+                                             place_fields=["country", "geo"],
+                                             max_results=10,
+                                             expansions='pinned_tweet_id'
+                                             )
 
     # tweet specific info
     print(len(tweets.data))
@@ -70,22 +71,32 @@ def twitter_method(query, next_token):
 
     # create a list of records
     tweet_info_ls = []
-
+    tweets_df = pd.DataFrame(tweet_info_ls)
     # iterate over each tweet and corresponding user details
     for tweet, user in zip(tweets.data, tweets.includes['users']):
         tweet_info = {
-            'created_at': tweet.created_at,
-            'text': tweet.text,
-            'source': tweet.source,
-            'geo': tweet.geo,
-            'id' : tweet.id,
+            # 'created_at': tweet.created_at,
+            # 'text': tweet.text,
+            # 'source': tweet.source,
+            # 'geo': tweet.geo,
+            # 'User_id' : tweet.id,
+            'attachments': tweet.attachments,
+            'author_id': tweet.author_id,
+            'created_at':	tweet.created_at,
+            'entities':	tweet.entities,
+            'geo':	tweet.geo,
+            'id':	tweet.id,
+            'source':	tweet.source,
+            'text':	tweet.text,
             'name': user.name,
             'username': user.username,
             'location': user.location,
             'verified': user.verified,
             'description': user.description,
             "entities": user.entities,
-            'country': user.location
+            'country': user.location,
+            #'reweet': retweet_count
+            
         }
         tweet_info_ls.append(tweet_info)
     # create dataframe from the extracted records
@@ -94,12 +105,14 @@ def twitter_method(query, next_token):
     # save dataset
     tweets_df.to_csv('c:\\temp\\file2.csv', header=True, index=True, mode='a')
     print(tweets.meta)
+    #print(tweets.data)
+    print(tweets)
     return tweets.meta["next_token"]
 
 
-next_token = twitter_method("nba","")
+next_token = twitter_method("nba", "")
 
-while(True):
-    next_token = twitter_method("nba","")
-    
-   # time.sleep(2100)
+# while(True):
+next_token = twitter_method("nba", "")
+
+# time.sleep(2100)
